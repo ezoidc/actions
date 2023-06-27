ff1dfa29a3f5220b34a6077863ced38faf456425
user <benoit.cote.jodoin@gmail.com>
2023-06-27 08:28:50 -0400

    v1

---
+++ kv/index.js
@@ -0,0 +1,25 @@
+module.exports = async ({ github, context, core, fetch }) => {
+  const { ORG } = process.env;
+  const audience = `https://oidc.bcj.io/${ORG}`;
+
+  const token = await core.getIDToken(audience);
+  const response = await fetch("https://oidc.bcj.io/api/v0/variables", {
+    method: "POST",
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  const body = await response.json();
+
+  if (!response.ok) {
+    return core.setFailed(body);
+  }
+
+  for (let { key, sensitive, value, envvar } of body.variables) {
+    if (sensitive) {
+      core.setSecret(value);
+    }
+
+    core.exportVariable(envvar, value);
+
+    core.info(`Set env ${envvar} to ${key} (sensitive=${sensitive})`);
+  }
+};
